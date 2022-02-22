e034ef4969003579ff9fd572b609b8c2451f72f8
Je Ar Becerro <rolandobecerrojr.doh@gmail.com>
2022-02-22 16:35:44 +0800

    Added server file from Feb 17, 2022 main branch | initialization

---
+++ mainServer_2-17-22/models/TempUser.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const User = new Schema({
+  username: {type: String},
+  password: {type: String},
+
+	name: {
+    first: {type: String},
+    mid: {type: String},
+    last: {type: String},
+  },
+
+  designation: {type: String},
+  status: {type: String},
+
+  office: {type: Schema.Types.ObjectId, ref: 'offices'},
+  role: {type: Schema.Types.ObjectId, ref: 'roles'},  
+
+});
+
+module.exports = mongoose.model('users', User);
---
+++ mainServer_2-17-22/routes/api/office.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const Office = require('../../controllers/office');
+
+const checkAuth = require('../middleware/checkAuth');
+const checkAdmin = require('../middleware/check-admin');
+
+router.get('/get', Office.get);
+
+//router.post('/new', checkAuth, checkAdmin, Office.new);
+
+router.post('/new', Office.new);
+router.post('/update', checkAuth, Office.update);
+
+module.exports = router;

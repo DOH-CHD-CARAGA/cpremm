1b99556849922106c5505b7ac8fd2c6ec3ea1abf
Je Ar Becerro <rolandobecerrojr.doh@gmail.com>
2022-02-22 16:31:28 +0800

    CryptoJS config added/edited

--- clientPortal/src/controller/crypto.js
+++ clientPortal/src/controller/crypto.js
@@ -1,16 +1,21 @@
-require('dotenv').config()
+import env from "react-dotenv"
+
 const CryptoJS = require("crypto-js")
 
 class Crypto{
 
     encrypt(text){
-        var ciphertext = CryptoJS.AES.encrypt(text, process.env.SECURITY_KEY ).toString();
-        return ciphertext
+        try {
+            const hashed = CryptoJS.AES.encrypt(text, env.SECURITY_KEY).toString();
+            return hashed
+        } catch (err) {
+            console.log(err.message)
+        }
     }
    
     decrypt(text){
         try {
-            var bytes  = CryptoJS.AES.decrypt(text, process.env.SECURITY_KEY)
+            var bytes  = CryptoJS.AES.decrypt(text, env.SECURITY_KEY)
             var originalText = bytes.toString(CryptoJS.enc.Utf8)
             return originalText
         } catch (err) {
